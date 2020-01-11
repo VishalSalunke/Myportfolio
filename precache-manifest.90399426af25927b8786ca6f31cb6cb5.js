812ced3360e7cc7706eaa3b512b3c9bb9eca0170
vishal <salunkevishal05@gmail.com>
2020-01-11 18:23:29 +0530

    Updates

---
+++ precache-manifest.90399426af25927b8786ca6f31cb6cb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae53c2fb53671e8334e197695bc262c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecccf2ae0a82af1efde",
+    "url": "/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "a2c43d7fbee813a59265",
+    "url": "/static/css/main.2a6b3386.chunk.css"
+  },
+  {
+    "revision": "0ecccf2ae0a82af1efde",
+    "url": "/static/js/2.dce06ded.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/static/js/2.dce06ded.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2c43d7fbee813a59265",
+    "url": "/static/js/main.acd5724f.chunk.js"
+  },
+  {
+    "revision": "443f4e4e0efbd37448c1",
+    "url": "/static/js/runtime-main.689fd073.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/static/media/todolist.ac82d97d.png"
+  },
+  {
+    "revision": "2619e19d8f6cbea745aab00afe335011",
+    "url": "/static/media/vishalSalunke.2619e19d.jpg"
+  }
+]);
